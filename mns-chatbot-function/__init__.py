7d995f1cc168b51c7c9db8d793349baedee42970
Ihab Wahbi <ihab.a.wahbi@gmail.com>
2024-02-23 00:15:50 +1030

    commit after remote build

--- mns-chatbot-function/__init__.py
+++ mns-chatbot-function/__init__.py
@@ -3,6 +3,7 @@ import logging
 import json
 import psycopg2
 import os
+from utils.openai.openai_manager import openai_handle_initial_msg
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -20,47 +21,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(json.dumps({"message": "The question appears to be empty. Please ask a meaningful question."}), mimetype="application/json", status_code=400)
     else:
         # Process the prompt and generate a response
-        response = get_material_info(prompt)
+        response = openai_handle_initial_msg(prompt)
         response_message = json.dumps({"message": response})
         return func.HttpResponse(response_message, mimetype="application/json", status_code=200)
-    
-def get_material_info(material_id):
-    # Database connection parameters from environment variables
-    conn_params = {
-        'dbname': os.getenv('DB_NAME'),
-        'user': os.getenv('DB_USER'),
-        'password': os.getenv('DB_PASS'),
-        'host': os.getenv('DB_HOST'),
-        'port': os.getenv('DB_PORT')
-    }
-    # SQL query to execute
-    query = """
-        SELECT material_description, plant, soh
-        FROM "material-soh"
-        WHERE material = %s;
-    """
-    
-    try:
-        # Connect to your database
-        print(conn_params)
-        conn = psycopg2.connect(**conn_params)
-        cursor = conn.cursor()
-        
-        # Execute the query
-        cursor.execute(query, (material_id,))
-        result = cursor.fetchall()
-        
-        # Convert query result to a more friendly format, if result is not empty
-        if result:
-            response = [{"material_description": row[0], "plant": row[1], "soh": str(row[2])} for row in result]
-        else:
-            response = []
-        
-        # Clean up
-        cursor.close()
-        conn.close()
-        
-        return response
-    except Exception as e:
-        print(f"Database query failed: {e}")
-        return None
